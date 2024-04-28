94667e894f40e512974177876b60d94a041ddb2e
Dementyev_Andrey <dem.an.a@yandex>
2024-04-28 14:27:27 +0500

    made table with aggregated values

--- src/cpu_monitoring/views.py
+++ src/cpu_monitoring/views.py
@@ -1,11 +1,50 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import SystemInfo
+from django.db.models import Max, Min, Avg
 
 
 def main(request):
-    system_info = SystemInfo.objects.all().order_by("-id")[:100]
-    context = {"rows": system_info}
+    system_info = SystemInfo.objects.all().order_by("-id")
+    aggregated = system_info.aggregate(Max("cpu_avg"), Min("cpu_avg"), Avg("cpu_avg"))
+    aggregated_from_last_100 = system_info[:100].aggregate(
+        Max("cpu_avg"), Min("cpu_avg"), Avg("cpu_avg")
+    )
+    context = {
+        "rows": system_info[:100],
+        "agregate": [
+            {
+                "id": "max_value",
+                "info": aggregated["cpu_avg__max"],
+                "title": "Максимальное значение в базе данных",
+            },
+            {
+                "id": "min_value",
+                "info": aggregated["cpu_avg__min"],
+                "title": "Минимальное значение в базе данных",
+            },
+            {
+                "id": "avg_value",
+                "info": aggregated["cpu_avg__avg"],
+                "title": "Среднее значение из в базе данных",
+            },
+            {
+                "id": "max_value_from_100",
+                "info": aggregated_from_last_100["cpu_avg__max"],
+                "title": " Максимальное значение из последних 100",
+            },
+            {
+                "id": "min_value_from_100",
+                "info": aggregated_from_last_100["cpu_avg__min"],
+                "title": "Минимальное значение из последних 100",
+            },
+            {
+                "id": "avg_value_from_100",
+                "info": aggregated_from_last_100["cpu_avg__avg"],
+                "title": "Среднее значение из последних 100",
+            },
+        ],
+    }
     return render(request, "table.html", context)
 
 
@@ -14,10 +53,10 @@ def get_new(request):
     # Далее логично былло бы использовать сериализатор, но в в задании DRF не значился
     result = [
         {
-            "created": o.get_str_data(),
-            "cpu": o.cpu_avg,
+            "created": info_object.get_str_data(),
+            "cpu": info_object.cpu_avg,
         }
-        for o in system_info
+        for info_object in system_info
     ]
     response = {"result": result}
     return JsonResponse(response)
